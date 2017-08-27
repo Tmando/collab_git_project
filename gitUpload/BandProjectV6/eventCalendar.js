e91973c2e546f9d3a05e35330c7447a9bf3ba591
Thomas Mandorfer <t.mandorfer@gmail.com>
2017-08-27 21:19:59 +0200

    update Page 2057

---
+++ gitUpload/BandProjectV6/eventCalendar.js
@@ -0,0 +1,156 @@
+eventsWorking = [{date:"07-11-2009", title:"", location:"Davis - Live Music Club",locationLink:"",text:"Kürschnergasse 9 1210 Wien",img:""},
+{date:"18-12-2010", title:"Pink Floyd Night", location:"bluegarage",locationLink:"",text:"Hinterleitenstraße 40 8523 Frauental",img:""},
+{date:"16-10-2010", title:"Pink Floyd Night", location:"Kulturhaus Enzenreith",locationLink:"",text:"Schulstraße 101 A-2640 Wörth",img:""},
+{date:"10-09-2010", title:"Pink Floyd Night", location:"Backstage",locationLink:"",text:"Brodtischgasse 16 A-2700 Wiener Neustadt",img:""},
+{date:"09-07-2010", title:"Strombauamt Rock Night", location:"Strombauamt Greifenstein",locationLink:"",text:"Hauptstraße 8 A-3433 Greifenstein Floyd Division Pensive Lane Neckslash",img:""},
+{date:"09-07-2010", title:"\"Pink Floyd meets Cream\" a Tribute Night mit: Floyd Division & Cream Revival Band", location:"Szene Wien",locationLink:"",text:"Hauffgasse 26 1110 Wien",img:""},
+{date:"12-02-2010", title:"", location:"Reigen - Live Music Club",locationLink:"",text:"Hadikgasse 62 1140 Wien ",img:""},
+{date:"09-12-2011", title:"Floyd Division & pensive lane", location:"machbar",locationLink:"",text:"Wasserzeile 35 3400 Klosterneuburg",img:""},
+{date:"09-12-2011", title:"", location:"Kulturhaus Pottschach",locationLink:"",text:"F. Samwaldstraße 4 A-2630 Pottschach",img:"img/Pottschach1_k.jpg"},
+{date:"24-09-2011", title:"Benefizkonzert zugunsten Marcel & Daniel", location:"Urania Wien",locationLink:"",text:"Uraniastraße 1 1010 Wien Special Guest: Ulli Bäer",img:""},
+{date:"17-09-2011", title:"", location:"The Spirit of 66",locationLink:"",text:"Place du Martyr 16 84800 Verviers Belgium",img:""},
+{date:"15-07-2011", title:"", location:"LIVE am Freitag im Zentrum (Fr. i. Z.)",locationLink:"",text:"Hauptplatz Traun 4050 Traun /OÖ",img:""},
+{date:"09-07-2011", title:"\"Bluegarage\" Closed Season Open Air", location:"bluegarage",locationLink:"",text:"Hinterleitenstraße 40 8523 Frauental",img:""},
+{date:"06-05-2011", title:"", location:"Reigen - Live Music Club",text:"Hadikgasse 62 1140 Wien",img:""},
+{date:"28-01-2011", title:"Pink Floyd meets U2 Floyd Division & Tribu2", location:"Szene Wien",text:"Hauffgasse 26 1110 Wien",img:""},
+{date:"17-11-2012", title:"Pink Floyd meets U2 Floyd Division & Tribu2", location:"Liveclub Tante JU",text:"An der Schleife 1 01099 Dresden Deutschland",img:"img/TanteJu2.jpg"},
+{date:"10-11-2012", title:"Pink Floyd meets U2 Floyd Division & Tribu2", location:"Kulturhaus Pottschach",text:"F. Samwaldstraße 4 A-2630 Pottschach Drei-jähriges Jubiläums Konzert von \"Floyd Division\" mit Special Guest Ulli Bäer und dem Schulchor MS Ternitz",img:"img/Pottschach.jpg"},
+{date:"16-02-2013", title:"Rock Tribute Night 2013 Floyd Division & Dire Straits-Project", location:"Szene Wien",text:"Hauffgasse 26 1110 Wien",img:"img/Szene2013.jpg"},
+{date:"06-07-2013", title:"\"Bluegarage\" Closed Season Open Air", location:"Bluegarage",text:"Hinterleitenstraße 40 8523 Frauental",img:"img/BlueGarage_2013k.jpg"},
+{date:"05-10-2013", title:"Pink Floyd meets Led Zeppelin", location:"SPINNEREI - Traun",text:"Graumannplatz 1 4050 Traun",img:"img/SpinnereiK.jpg"},
+{date:"25-10-2013", title:"Floyd Division & Tribu2", location:"EMS Lounge",text:"Dietrichgasse 25 1030 Wien",img:"img/EMS_Lounge_k.jpg"},
+{date:"01-03-2014", title:"5. Rock Tribute Night Floyd Division & fivefifteen (The Who Tribute)", location:"Szene Wien ",text:"Hauffgasse 26 1110 Wien",img:"img/Szene2014.jpg"},
+{date:"24-05-2014", title:"Benefizkonzert zugunsten \"Music 4 Charity\"", location:"Ottakringer Brauerei",text:"Ottakringer Straße 91 1160 Wien",img:""},
+{date:"25-05-2014", title:"Donaukanaltreiben Wien", location:"City-Beach Bühne",text:"Wien",img:""},
+{date:"05-07-2014", title:"Bluegarage Closed Season Open Air", location:"Bluegarage",text:"Hinterleitenstraße 40 8523 Frauental",img:""},
+{date:"20-09-2014", title:"", location:"Liveclub Tante JU",text:"An der Schleife 1 01099 Dresden Deutschland",img:""},
+{date:"04-10-2014", title:"", location:"SPINNEREI - Traun",text:"Graumannplatz 1 4050 Traun",img:""},
+{date:"29-10-2014", title:"", location:"Rahofer Bräu",text:"Dumba Park 1 2523 Tattendorf",img:"g"},
+{date:"07-11-2014", title:"Floyd Division mit Kinderchor MS Ternitz", location:"Kulturhaus Pottschach",text:"Franz Samwald Str. 4 2630 Ternitz",img:""},
+{date:"25-04-2015", title:"Seventies Forever Floyd Division Grand Zeppelin Alex Zilinski Trio", location:"Bank Austria Gasometer",text:"Guglgasse 8 1110 Wien",img:"img/seventies_forever_k.jpg"},
+{date:"29-05-2015", title:"Donaukanaltreiben", location:"Citybeachbühne",text:"Wien",img:"CityBeach_Logo.jpg"},
+{date:"30-05-2015", title:"", location:"Rock Arena",text:"Solenbergstrasse 35 8207 Schaffhausen/Herblingen Switzerland",img:"img/RockArena_k.jpg"},
+{date:"04-07-2015", title:"Bluegarage Closed Season Open Air", location:"Bluegarage",text:"Hinterleitenstraße 40 8523 Frauental",img:""},
+{date:"02-10-2015", title:"", location:"Eboardmuseum",text:"Florian Gröger Straße 20 A 9020 Klagenfurt",img:"img/Eboard_K.jpg"},
+{date:"06-11-2015", title:"", location:"Rahofer Bräu",text:"Dumba Park 1 A 2325 Tattendorf",img:""},
+{date:"07-11-2015", title:"", location:"Kulturverein Röda",text:"Gaswerkgasse 2 A 4400 Steyr",img:""},
+{date:"04-12-2015", title:"", location:"Conrad Sohm",text:"Boden 1 6850 Dornbirn",img:""},
+{date:"27-05-2016", title:"", location:"Hoyerswerda Kulturfabrik",text:"Braugasse 1, 02977 Hoyerswerda Deutschland",img:""},
+{date:"28-05-2016", title:"", location:"Liveclub Tante JU",text:"An der Schleife 1 01099 Dresden Deutschland",img:"img/TanteJu2016_k.jpg"},
+{date:"09-07-2016", title:"Bluegarage Closed Season Open Air", location:"Bluegarage",text:"Hinterleitenstraße 40 8523 Frauental",img:""},
+{date:"23-09-2016", title:"", location:"Liveclub Tante JU",text:"An der Schleife 1 01099 Dresden Deutschland",img:"img/TanteJu2.jpg"},
+{date:"17-11-2012", title:"Pink Floyd meets U2 Floyd Division & Tribu2", location:"Conrad Sohm",text:"Boden 1 6850 Dornbirn",img:""},
+{date:"24-09-2016", title:"", location:"Rock Arena",text:"Solenbergstrasse 35 8207 Schaffhausen/Herblingen Switzerland ",img:"img/RockArena2_k.jpg"},
+{date:"15-10-2016", title:"Covernight mit Thunderballs (AC/DC) und The Stonez (Rolling Stones)", location:"Orpheum",text:"Steigenteschgasse 94 b 1220 Wien",img:"img/Covernight2016K.jpg"},
+{date:"21-10-2016", title:"", location:"PPC Graz",text:"Neubaugasse 5 A 8020 Graz",img:""},
+{date:"04-11-2016", title:"", location:"Rahofer Bräu",text:"Dumba Park 1 A 2325 Tattendorf",img:""},
+{date:"30-12-2016", title:"", location:"Greimhalle",text:"St. Peter 828843 St. Peter am Kammersberg",img:""},
+{date:"20-01-2017", title:"Berkeller Reichenbach presents Floyd Division live", location:"Neuberinhaus",text:"Weinholdstraße 7 D-08468 Reichenbach im Vogtland",img:""},
+{date:"21-01-2017", title:"", location:"Plutonium Klub",text:"Industriestraße 3, D- 94342 Straßkirchen",img:""},
+{date:"04-02-2017", title:"", location:"SimmCity",text:"Simmeringer Hauptstraße 96A A - 1110 Wien",img:"img/SimmFlyer_k.jpg"},
+{date:"26-05-2017", title:"", location:"Sargfabrik",text:"Goldschlagstraße 169 1140 Wien",img:""},
+{date:"08-07-2017", title:"Bluegarage Closed Season Open Air", location:"Bluegarage",text:"Hinterleitenstraße 40 8523 Frauental",img:""},
+{date:"21-07-2017", title:"LIVE am Freitag im Zentrum (Fr. i. Z.)", location:"Spinnerei",text:"Hauptplatz Traun 4050 Traun /OÖ",img:""},
+{date:"04-08-2017", title:"", location:"Burg Mylau",text:"Burg 1 D-08499 Reichenbach",img:""},
+{date:"12-08-2017", title:"Echt Scharf Solingen", location:"Open Air Festival D - Solingen",text:"",img:""},
+{date:"14-08-2017", title:"", location:"Thermalbad Fischau",text:"Hauptstraße 10 2721 Bad Fischau- Brunn",img:""},
+{date:"16-09-2017", title:"", location:"Kulturhaus Pottschach ",text:"Franz Samwald Straße 4 2630 Ternitz	",img:"img/Pottschach_2017_k.jpg"},
+{date:"22-09-2017", title:"", location:"Eboardmuseum",text:"Florian Gröger Straße 20 A 9020 Klagenfurt",img:"img/2017_EBOARDMUSEUM_k.jpg"},
+{date:"28-10-2017", title:"", location:"Rock Arena",text:"Solenbergstrasse 35 8207 Schaffhausen/Herblingen Switzerland ",img:""},
+{date:"10-11-2017", title:"", location:"Rahofer Bräu",text:"Dumba Park 1 A 2325 Tattendorf",img:""},
+{date:"24-11-2017", title:"Kultur im Zentrum - Spielberg", location:"",text:"",img:""}
+];
+
+yearArray = Array();
+function getYears(){
+  for(let i= 0; i<eventsWorking.length;i++){
+    date = eventsWorking[i].date.split("-");
+    console.log(date[2]);
+    if(yearArray.includes(Number(date[2])) == false){
+      yearArray.push(Number(date[2]));
+      document.getElementById("yearSelector").innerHTML += "<option value=\""+date[2]+"\">"+date[2]+"</option>";
+    }
+
+  }
+
+}
+
+function returnEvents(year){
+  year = Number(year);
+  eventArray = new Array();
+  for(let i= 0; i<eventsWorking.length;i++){
+    date = eventsWorking[i].date.split("-");
+    if(Number(date[2]) == year){
+      eventArray.push(eventsWorking[i]);
+    }
+  }
+
+  return eventArray;
+}
+function getEvents(){
+  var x = document.getElementById("yearSelector").value;
+  parseEvents(x);
+}
+function parseEvents(year){
+  eventArray = returnEvents(year);
+  tag = "";
+  tag += "<h1>"+year+"</h1>";
+  for(let i = 0;i<eventArray.length;i++){
+    if(eventArray[i].img == ""){
+      tag+="<div class=\"row\" style=\"margin-bottom:5vh\">";
+        tag +="<div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-3 col-xl-3\">";
+          tag += eventArray[i].date;
+        tag += "</div>";
+        tag +="<div class=\"col-lg-6 col-md-6 col-sm-6 col-xs-6 col-xl-6\">";
+          if(eventArray[i].title != ''){
+          tag += eventArray[i].title;
+          tag += "<br \>";
+          }
+          if(eventArray[i].location != ''){
+            tag += eventArray[i].location;
+            tag += "<br \>";
+          }
+          if(eventArray[i].text != ''){
+            tag += eventArray[i].text;
+            tag += "<br \>";
+          }
+        tag += "</div>";
+      tag+="</div>";
+    }else{
+      tag+="<div class=\"row\" style=\"margin-bottom:5vh\">";
+        tag +="<div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-3 col-xl-3\">";
+          tag += eventArray[i].date;
+        tag += "</div>";
+        tag +="<div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-3 col-xl-3\">";
+          if(eventArray[i].title != ''){
+          tag += eventArray[i].title;
+          tag += "<br \>";
+          }
+          if(eventArray[i].title != ''){
+            tag += eventArray[i].location;
+            tag += "<br \>";
+          }
+          if(eventArray[i].text != ''){
+            tag += eventArray[i].location;
+            tag += "<br \>";
+          }
+        tag += "</div>";
+        tag +="<div class=\"col-lg-3 col-md-3 col-sm-3 col-xs-3 col-xl-3\">";
+        tag+="<img class=\"img-responsive\""+"src=\""+eventArray[i].img+"\">"
+        tag += "</div>";
+      tag+="</div>";
+    }
+  }
+  document.getElementById("events").innerHTML = tag;
+}
+
+/*
+function parseEvent(year){
+  tag = "";
+  eventArray = returnEvents(year)
+  for(let i= 0;i<eventArray.length;i++){
+    if(eventArray[i].img!=""){
+    }
+  }
+}
+*/
